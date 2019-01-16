cba0093b362af2bf3806682df393ad5c741c1ea6
JurekInholland <jurekbaumann@me.com>
2019-01-16 21:02:59 +0100

    Add project files.

---
+++ Assignment3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+
+        }
+    }
+}
